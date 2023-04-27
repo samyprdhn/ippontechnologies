24be22fbdd32a86e6dbbafa5e2d3188980396cf3
samyprdhn <samyprdhn@gmail.com>
2023-04-26 20:45:45 -0400

    feat: initial web3 deploy

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import IpponSquare from './IpponSquare.svg';
 import IpponTextLogo from './IpponText.svg';
+import github from './github.svg';
+import instagram from './instagram.svg';
+import linkedin from './linkedin.svg';
 import './App.css';
 
 function App() {
@@ -11,7 +14,20 @@ function App() {
           <br></br>
         <img src={IpponTextLogo} className="Ippon-logo" alt="IpponWording" />
       </header>
+
+
+
+        <footer className="App-footer">
+            <a href={"https://www.instagram.com/ippon_technologies"}><img src={instagram} alt={"Ippon Instagram"}/></a>
+            <p>&nbsp;</p>
+            <a href={"https://github.com/ippontech"}><img src={github} alt={"Ippon GitHub"}/></a>
+            <p>&nbsp;</p>
+            <a href={"https://www.linkedin.com/company/ippon-technology"}><img src={linkedin} alt={"Ippon Linkedin"}/></a>
+        </footer>
+
+
     </div>
+
   );
 }
 
